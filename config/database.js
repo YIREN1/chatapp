089e7504e03b4f45480f94ebdd9983c01de9b8d4
Yi Ren <y78ren@uwaterloo.ca>
2019-01-05 20:54:30 -0500

    initial

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  database: 'mongodb://localhost:27017/authapp',
+  // database: 'mongodb://admin:bookshare92@ds119523.mlab.com:19523/bookshareapp',
+  secret: 'mybreathtakingastonishingawe-inspiringsecret'
+}
