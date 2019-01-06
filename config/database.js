0c64f98625dbfbb2d9ce2cbc96484443be4b5f63
Yi Ren <y78ren@uwaterloo.ca>
2019-01-06 07:54:50 -0500

    setup heroku mongodb

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/authapp',
-  // database: 'mongodb://admin:bookshare92@ds119523.mlab.com:19523/bookshareapp',
+  // database: 'mongodb://localhost:27017/authapp',
+  database: 'mongodb://yren:123456a@ds113692.mlab.com:13692/authapp',
   secret: 'mybreathtakingastonishingawe-inspiringsecret'
 }
