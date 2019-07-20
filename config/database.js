61e26057f7c62f12cf68770d73bce7402bdcc099
Yi Ren <jamesren1998@gmail.com>
2019-07-20 15:48:32 -0400

    hide key

--- config/database.js
+++ config/database.js
@@ -4,6 +4,5 @@ const mongoPassword = process.env.MONGO_PASSWORD;
 module.exports = {
   // database: 'mongodb://localhost:27017/authapp',
   database: `mongodb://${mongoUser}:${mongoPassword}@ds113692.mlab.com:13692/authapp`,
-  secret:
-    'm4%^$56$%ybrea234c34thtak34c45vin#$%^#%gastonishingawe-insp%^>"?.v54iringsecret',
+  secret: process.env.MONGO_SECRET,
 };
