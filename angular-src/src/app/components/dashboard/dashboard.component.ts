1fd3eeaea1000665b8c6333d8254d627743eb462
Yi Ren <jamesren1998@gmail.com>
2019-07-21 00:38:46 -0400

    config

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -3,23 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'app-dashboard',
 	templateUrl: './dashboard.component.html',
-	styleUrls: [ './dashboard.component.css' ]
+	styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 	constructor() {
-		const url = './docs/test.pdf';
-		let pdfDoc = null;
-		let pageNum = 1;
-		let pageIsRendering = false;
-		let pageNumIsPending = null;
 
-		const scale = 1.5;
-		const canvas = document.querySelector('#pdf-render');
-		// const context = canvas.getContext('2d');
 	}
 
-	ngOnInit() {}
-	renderPage(num) {
-		// pdfjsLib.getDocument(url).promise.then((pdfDoc_) => {});
-	}
+	ngOnInit() { }
+
 }
